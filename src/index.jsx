2cc6eb54d06b66bf4f850e61cb65b8975b6c8c34
Blaxion <khibaziev.f@gmail.com>
2021-06-08 22:15:20 +0200

    commit 2

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import "./sass/App.sass"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
